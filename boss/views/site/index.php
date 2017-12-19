80eb543408c94244d6a39591ea7b59702e83b133
zzs10086 <563315963@qq.com>
2017-12-19 22:42:08 +0800

    modify

--- boss/views/site/index.php
+++ boss/views/site/index.php
@@ -10,74 +10,64 @@ $this->title = '管理后台-首页';
 $this->params['current_name'] = '首页';
 ?>
 
-<div class="content-wrapper y_content-wrapper">
-     <!-- Content Header (Page header) -->
-     <section class="content-header">
-          <h1>
-               个人信息
-               <small>修改个人信息</small>
-          </h1>
-     </section>
+<div class="container-fluid">
+     <hr>
 
-     <!-- Main content -->
-     <section class="content">
-          <div class="box box-info">
-               <div class="box-header with-border">
-                    <h3 class="box-title">修改个人信息</h3>
-               </div>
-               <div class="box-body">
-                    <?php $form = ActiveForm::begin([
-                         'action' => '',
-                         'options' => [
-                              'class' => 'form-horizontal'
-                         ]
-                    ]); ?>
-
-                    <?= $form->field($model, 'id',['template'=>'{input}{error}'])->hiddenInput()?>
-                    <div class="form-group">
-                         <?= $form->field($model, 'nick_name',[
-                              'template'=>'{label}<div class="col-sm-10 col-lg-5">{input}{error}</div>'
-                         ])->textInput([
-                              'class'=>'form-control',
-                              'maxlength' => 30
-                         ])->label('登录名称',[
-                              'class'=>'col-sm-2 control-label'
-                         ]) ?>
+     <div class="row-fluid">
+          <div class="span12">
+               <div class="widget-box">
+                    <div class="widget-title"> <span class="icon"> <i class="icon-list"></i> </span>
+                         <h5>首页</h5>
                     </div>
-                    <div class="form-group">
-                         <?= $form->field($model, 'real_name',[
-                              'template'=>'{label}<div class="col-sm-10 col-lg-5">{input}{error}</div>'
-                         ])->textInput([
-                              'class'=>'form-control',
-                              'maxlength' => 30
-                         ])->label('真实名称',[
-                              'class'=>'col-sm-2 control-label'
-                         ]) ?>
-                    </div>
-                    <div class="form-group">
-                         <?= $form->field($model, 'password',[
-                              'template'=>'{label}<div class="col-sm-10 col-lg-5">{input}{error}</div>'
-                         ])->passwordInput([
-                              'value'=>'',
-                              'class'=>'form-control',
-                              'maxlength' => 30
-                         ])->label('密码',[
-                              'class'=>'col-sm-2 control-label'
-                         ]) ?>
-                    </div>
+                    <div class="widget-content">
+
+                         <?php $form = ActiveForm::begin([
+                              'action' => '',
+                              'options' => [
+                                   'class' => 'form-horizontal'
+                              ],
+                              'fieldConfig' => [
+                                   'template' => "{label}\n<div class=\"controls\">{input}</div>\n<span class=\"help-inline\">{error}</span>",
+                                   'labelOptions' => ['class' => 'control-label'],
+                              ]
+                         ]); ?>
+
+                         <?= $form->field($model, 'id',['template'=>'{input}{error}'])->hiddenInput()?>
+                         <div class="form-group">
+                              <?= $form->field($model, 'nick_name')->textInput([
+                                   'class'=>'form-control',
+                                   'maxlength' => 30
+                              ])->label('登录名称',[
+                                   'class'=>'control-label'
+                              ]) ?>
+                         </div>
+                         <div class="form-group">
+                              <?= $form->field($model, 'real_name')->textInput([
+                                   'class'=>'form-control',
+                                   'maxlength' => 30
+                              ])->label('真实名称') ?>
+                         </div>
+                         <div class="form-group">
+                              <?= $form->field($model, 'password')->passwordInput([
+                                   'class'=>'form-control',
+                                   'maxlength' => 30
+                              ])->label('密码') ?>
+                         </div>
 
 
-                    <div class="form-group">
-                         <div class="col-sm-offset-2 col-sm-10" style="padding-top: 20px;">
-                              <?= Html::submitButton('保存', ['class' => 'btn btn-warning', 'id' => 'save']) ?>
+                         <div class="form-group">
+                              <div class="col-sm-offset-2 col-sm-10" style="padding-top: 20px;">
+                                   <?= Html::submitButton('保存', ['class' => 'btn btn-success', 'id' => 'save']) ?>
 
+                              </div>
                          </div>
                     </div>
-               </div>
-               <?php ActiveForm::end(); ?>
-          </div>
+                    <?php ActiveForm::end(); ?>
 
-     </section>
-     <!-- /.content -->
+               </div>
+          </div>
+     </div>
 </div>
 
+
+</div>
